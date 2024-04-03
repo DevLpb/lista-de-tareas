2bb76d04c5f08e25fb2c69ddc77bc08f4d4861ff
Leonel <leonelpiedrabuena04@gmail.com>
2024-04-03 12:21:12 -0300

    Avances en lógica de guardado
    Se hicieron algunos cambios en funciones para guardar tareas en el localStorage y mostrarlas en la página. Se agregaron algunos elementos HTML y clases CSS.

--- app.js
+++ app.js
@@ -8,20 +8,49 @@ const dialog = document.getElementById("form-dialog");
 const title = document.getElementById("title");
 const date = document.getElementById("date");
 const description = document.getElementById("description");
+const editBtn = document.getElementById("edit-task");
+const deleteBtn = document.getElementById("delete-task");
 
-function mostrarInfo() {
+let tasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+
+//Función para leer elementos guardados, y elegir mostrar o no el aviso de información.
+
+function showInfo() {
   if (tasks.length !== 0) {
     info.classList.add("hidden");
   } else {
     info.classList.remove("hidden");
   }  
 }
+function comprobarInfo() {
+  showInfo();
+  if (tasks) {
+    tasks.forEach(task => {
+      createElement(task);
+    });
+  }
+}
 
-let tasks = [];
+//Función para crear un nuevo elemento HTML
+function createElement(task) {
 
-mostrarInfo();
+  let taskDiv = document.createElement("div");
+  taskDiv.classList.add("task-div");
+  taskDiv.innerHTML = `
+  <h2 id="task-title">${task.title}</h2>
+  <p id="task-date">${task.date}</p>
+  <p id="task-description">${task.description}</p>
+  <div class="buttons-div">
+    <button id="edit-task">Editar</button>
+    <button id="delete-task">Borrar</button>
+    </div>
+  `;
+container.insertBefore(taskDiv, addTask);
+}
 
-// Al hacer click en el texto para agregar una nueva tarea, se abre el dialog con el formulario dentro.
+comprobarInfo();
+
+//Al hacer click en el texto para agregar una nueva tarea, se abre el dialog con el formulario dentro.
 addTask.addEventListener('click', openDialog);
 
 function openDialog() {
@@ -56,8 +85,8 @@ document.querySelector('form').addEventListener('submit', (event) => {
     alert("Tarea guardada");
     clearInputs();
     dialog.close();
-    crearElemento();
-    mostrarInfo();
+    addElement(task);
+    showInfo();
   });
 
 //Función para limpiar Inputs. En pruebas.
@@ -67,8 +96,8 @@ document.querySelector('form').addEventListener('submit', (event) => {
     description.value = "";
   }
   
-  //Función para crear un nuevo elemento HTML
-  function crearElemento() {
+  //Función para adicionar un nuevo elemento HTML
+  function addElement(task) {
 
     let taskDiv = document.createElement("div");
     taskDiv.classList.add("task-div");
@@ -76,6 +105,10 @@ document.querySelector('form').addEventListener('submit', (event) => {
     <h2 id="task-title">${tasks[tasks.length - 1].title}</h2>
     <p id="task-date">${tasks[tasks.length - 1].date}</p>
     <p id="task-description">${tasks[tasks.length - 1].description}</p>
+    <div class="buttons-div">
+    <button id="edit-task">Editar</button>
+    <button id="delete-task">Borrar</button>
+    </div>
     `;
  container.insertBefore(taskDiv, addTask);
   }
