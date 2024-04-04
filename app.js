2758dcff0d32535742b04748d05be8b058ea4f24
Leonel <leonelpiedrabuena04@gmail.com>
2024-04-04 18:15:20 -0300

    Lógica terminada
    Finalizado el desarrollo de la lógica de creación, edición y eliminación de tareas del localStorage. El DOM se actualiza correctamente y está sujeto a mejoras visuales.

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const description = document.getElementById("description");
 
 let tasks = JSON.parse(localStorage.getItem("myTasks")) || [];
 
+let editMode = false;
+
+let currentTaskTitle = null;
 //Función para leer elementos guardados, y elegir mostrar o no el aviso de información.
 
 function showInfo() {
@@ -20,7 +23,7 @@ function showInfo() {
     info.classList.remove("hidden");
   }  
 }
-function comprobarInfo() {
+function getInfo() {
   showInfo();
   if (tasks) {
     tasks.forEach(task => {
@@ -29,24 +32,37 @@ function comprobarInfo() {
   }
 }
 
-//Función para crear un nuevo elemento HTML
+//Función para crear un nuevo elemento HTML.
 function createElement(task) {
+
+  // Cambia el formato de la fecha a DD-MM-YYYY.
+  // Se ingresan los números.
+  let date = new Date(task.date);
+
+  // Se asignan los valores correspondientes.
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  // Se inicializa la fecha con el nuevo formato.
+  let formattedDate = `${day}-${month}-${year}`
+
 
   let taskDiv = document.createElement("div");
   taskDiv.classList.add("task-div");
   taskDiv.innerHTML = `
   <h2 id="task-title">${task.title}</h2>
-  <p id="task-date">${task.date}</p>
+  <p id="task-date">${formattedDate}</p>
   <p id="task-description">${task.description}</p>
   <div class="buttons-div">
-    <button id="edit-task">Editar</button>
+    <button id="edit-task" data-title="${task.title}">Editar</button>
     <button id="delete-task" data-title="${task.title}">Borrar</button>
     </div>
   `;
 container.insertBefore(taskDiv, addTask);
 }
 
-comprobarInfo();
+getInfo();
 
 //Al hacer click en el texto para agregar una nueva tarea, se abre el dialog con el formulario dentro.
 addTask.addEventListener('click', openDialog);
@@ -65,11 +81,41 @@ cancelBtn.addEventListener('click', () => {
     dialog.close();
 })
 
-//Botón de confirmación. En pruebas.
+//Botón de confirmación.
+
+// Previene el reinicio de la página.
 document.querySelector('form').addEventListener('submit', (event) => { 
     event.preventDefault(); 
 
-    //Creación de objeto y asignación de valores de input. Inclusión en array de tareas.
+    // Verifica si se está editando la tarea y ejecuta el código.
+    if (editMode) {
+
+      // Busca el índice de la tarea actual.
+      index = tasks.findIndex(task => task.title === currentTaskTitle);
+
+      if (index !== -1) {
+        tasks[index] = {
+          title: title.value,
+          date: date.value,
+          description: description.value
+        }
+
+        // Actualiza el elemento en localStorage.
+        localStorage.setItem("myTasks", JSON.stringify(tasks));
+
+        // Elimina todos los elementos del DOM y vuelve a actualizarlos con la nueva información del localStorage.
+        document.querySelectorAll('.task-div').forEach(e => e.remove());
+        getInfo();
+        alert("Tarea editada");
+        dialog.close();
+
+        // Actualiza las variables del modo de edición y el título de la tarea actual.
+        editMode = false;
+        currentTaskTitle = null;
+      }
+    } else {
+
+      //Creación de objeto y asignación de valores de input. Inclusión en array de tareas.
     let task = {
       title: title.value,
       date: date.value,
@@ -84,17 +130,43 @@ document.querySelector('form').addEventListener('submit', (event) => {
     alert("Tarea guardada");
     showInfo();
     clearInputs();
-    dialog.close();
+    dialog.close(); 
+    }
   });
+
+  //Despliegue de cuadro de edición.
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', event => {
+    // Comprueba si el evento se originó desde un botó de editar y ejecuta la función.
+    if (event.target.id === 'edit-task') {
+
+      //Actualiza la variable del modo de edición.
+      editMode = true;
+      
+      //Actualiza la variable del título la tarea actual.
+      currentTaskTitle = event.target.getAttribute('data-title');
+      
+      //Busca el índice de la tarea actual y asigna los valores a los inputs correspondientes.
+      let index = tasks.findIndex(task => task.title === currentTaskTitle);
+
+      if (index !== -1) {
+        title.value = tasks[index].title;
+        date.value = tasks[index].date;
+        description.value = tasks[index].description;
+        openDialog();
+      }
+    }
+  })
+})
 
   //Botón de eliminar.
 
   document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('click', event => {
-        // Comprueba si el evento se originó desde un botón de borrar y ejecutamos la función.
+        // Comprueba si el evento se originó desde un botón de borrar y ejecuta la función.
         if (event.target.id === 'delete-task') {
-          // Obtiene el título de la tarea correspondiente al botón presionado.
-          let currentTaskTitle = event.target.getAttribute('data-title');
+          // Obtiene el título de la tarea correspondiente al botón presionado y lo guarda en una variable.
+          currentTaskTitle = event.target.getAttribute('data-title');
 
           // Busca el índice de la tarea actual.
           let index = tasks.findIndex(task => task.title === currentTaskTitle );
@@ -104,10 +176,18 @@ document.querySelector('form').addEventListener('submit', (event) => {
             tasks.splice(index, 1);
           // Actualiza el localStorage con el nuevo array de tareas.
             localStorage.setItem("myTasks", JSON.stringify(tasks));
-          //Elimina los elementos padres del botón en el DOM.
+          // Elimina los elementos padres del botón en el DOM.
             event.target.parentNode.parentNode.remove();
+
+          // Resetea la variable que guarda el título de la tarea actual.
+            currentTaskTitle = null;
+
+          // Informa la situación.
+            alert("Tarea eliminada");
+            showInfo();
+          } else {
+            alert("La tarea no existe");
           }
-          alert("Tarea eliminada");
         }
     });
 });
